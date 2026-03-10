04136c7e77340e11fe46fca7f147b616716704ef
sks4566 <82690526+sks4566@users.noreply.github.com>
2026-03-10 21:01:37 +0530

    merge intervals

---
+++ MergeIntervals/NonOverlappingIntervals.java
@@ -0,0 +1,23 @@
+package MergeIntervals;
+
+import java.util.Arrays;
+
+public class NonOverlappingIntervals {
+    public static void main(String[] args) {
+        System.out.println( new NonOverlappingIntervals().eraseOverlapIntervals(new int[][]{{1, 2}, {2, 3}, {3, 4}, {1, 3}}));
+    }
+
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        int count = 0;
+        int end = Integer.MIN_VALUE;
+        for (int[] interval : intervals) {
+            if (interval[0] >= end) {
+                end = interval[1];
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+}
